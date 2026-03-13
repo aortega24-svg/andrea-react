6180d012f4e22754eeee9156675bf70884b5d05e
Andrea Ortega <aortega24@xarxa.fedac.cat>
2026-03-13 19:06:14 +0100

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 
 import './App.css'
 
-import Exercici1 from './Components/tasca1/Exercici1.jsx'
-import Exercici2 from './Components/tasca1/Exercici2.jsx'
-import Exercici3 from './Components/tasca1/Exercici3.jsx'
-import Exercici4 from './Components/tasca1/Exercici4.jsx'
+import tasca_2 from './Components/tasca2/tasca_2.jsx'
 import tasca_2 from './Components/tasca2/tasca_2.jsx'
 
 
@@ -13,10 +10,6 @@ function App() {
 
   return (
      <div>
-      <Exercici1/>
-      <Exercici2/>
-      <Exercici3/>
-      <Exercici4/>
       <tasca_2/>
      </div>
       
